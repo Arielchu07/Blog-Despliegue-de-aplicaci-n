7589a0500eefccc273e715ffc8984a93d9a2e28a
ari <ari@example.com>
2023-11-30 13:37:15 -0300

    Despliega

--- python_heroku_task/settings.py
+++ python_heroku_task/settings.py
@@ -82,12 +82,11 @@ WSGI_APPLICATION = 'python_heroku_task.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-       dj_database_url.config(
-    default='postgresql://postgres:postgres@localhost/postgres'
-    conn_max_age=600
-       )
-    }
+    'default': dj_database_url.config(
+        # Feel free to alter this value to suit your needs.
+        default='postgresql://postgres:postgres@localhost:5432/mysite',
+        conn_max_age=600
+    )
 }
 
 
